eb6d087b69b74b482e67499dad2073b27ba7a0ff
Konstantin Sumatokhin <ksumatokhin@gmail.com>
2025-12-15 23:05:40 +0200

    Вторая и третья домашка

---
+++ Task3_10/Program.cs
@@ -0,0 +1,39 @@
+﻿//Застройщик построил n домов. Вывести фразу «Мы построили n домов», обеспечив
+//правильное согласование числа со словом «дом», например:
+//20 — «Мы построили 20 домов»,
+//32 — «Мы построили 32 дома»,
+//41 — «Мы построили 41 дом».
+
+namespace Task3_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nСколько домов построил застройщик: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 100 >= 11 && num % 100 < 15)
+            {
+                Console.WriteLine("Мы построили {0} домов", num);
+            }
+            else
+            {
+                switch (num % 10)
+                {
+                    case 2:
+                    case 3:
+                    case 4:
+                        Console.WriteLine("Мы построили {0} дома", num);
+                        break;
+                    case 1:
+                        Console.WriteLine("Мы построили {0} дом", num);
+                        break;
+                    default: // 0,5-9
+                        Console.WriteLine("Мы построили {0} домов", num);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Task4_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Вводится натуральное число n. Найти n! Например, 6! = 1 * 2 * 3 * 4 * 5 * 6.
+
+namespace Task4_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nВводите натуральное число: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int n_factorial = 1;
+
+            for (int i = 1; i <= num; i++)
+            {
+                n_factorial *= i;
+            }
+
+            Console.WriteLine("Натуральный логарифм числа {0} - {1}", num, n_factorial);
+        }
+    }
+}
