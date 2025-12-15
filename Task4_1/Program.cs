b9fe0a4146200f3d788b021513bb894fee73c61a
Konstantin Sumatokhin <ksumatokhin@gmail.com>
2025-12-15 23:36:53 +0200

    Факториал

--- Task4_1/Program.cs
+++ Task4_1/Program.cs
@@ -1,21 +1,30 @@
 ﻿// Вводится натуральное число n. Найти n! Например, 6! = 1 * 2 * 3 * 4 * 5 * 6.
 
+using System;
+
 namespace Task4_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("\nВводите натуральное число: ");
-            int num = Convert.ToInt32(Console.ReadLine());
-            int n_factorial = 1;
+            Console.Write("\nВведите положительное натуральное число: ");
+            int number;
+            bool success = int.TryParse(Console.ReadLine(), out number);
+            long factorial = 1;
 
-            for (int i = 1; i <= num; i++)
+            if (success && number >= 0)
             {
-                n_factorial *= i;
+                for (int i = 2; i <= number; i++)
+                {
+                    factorial *= i;
+                }
+                Console.WriteLine("Факториал числа {0} - {1}", number, factorial);
             }
-
-            Console.WriteLine("Натуральный логарифм числа {0} - {1}", num, n_factorial);
+            else
+            {
+                Console.WriteLine("Введенное значение должно быть положительным числом");
+            }
         }
     }
 }
