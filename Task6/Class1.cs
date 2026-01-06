c34b7939f2a0dafa830cdc56e0262d3792f698fc
Konstantin Sumatokhin <ksumatokhin@gmail.com>
2026-01-06 14:30:49 +0200

    Домашка по стрингам

---
+++ Task6/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task6
+{
+    class Cat
+    {
+        public const int countFeet = 4;
+
+        public readonly bool wool = true;
+
+        public string Name { get; set; }
+        public string Color { get; set; }
+        int age;
+
+        public int Age
+        {
+            set
+            {
+                if (value > 0)
+                    age = value;
+            }
+            get {return age; }
+        }
+
+        public Cat(string name,  string color, int age, bool wool)
+        {
+            Name = name;
+            Color = color;
+            Age = age;
+            this.wool = wool;
+        }
+    }
+}
---
+++ Task6/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите предложение: ");
+            string sentence = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(sentence))
+            {
+                Console.WriteLine("Вы ввели пустую строку.");
+                return;
+            }
+            string[] words = sentence.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            if (words.Length == 0)
+            {
+                Console.WriteLine("Не найдено ни одного слова.");
+                return;
+                // Разделяем строку на слова по пробелам
+
+            }
+
+            // Находим самое длинное слово
+            string longestWord = words[0];
+
+            for (int i = 1; i < words.Length; i++)
+            {
+                if (words[i].Length > longestWord.Length)
+                {
+                    longestWord = words[i];
+                }
+            }
+
+            Console.WriteLine($"Самое длинное слово: \"{longestWord}\" (длина: {longestWord.Length})");
+        }
+    }
+}
