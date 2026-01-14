962523b1e77f06f2f9d46f2ce24f6ff73100bf4a
Konstantin Sumatokhin <ksumatokhin@gmail.com>
2026-01-14 17:01:57 +0200

    Игра «Угадай число»

---
+++ Guess_number/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Channels;
+
+namespace Guess_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Игра \"Угадай число\"");
+            Console.WriteLine("Компьютер загадал число от 1 до 1000.");
+
+            Random random = new Random();
+            int secret = random.Next(1, 1001);
+            int attempts = 0;
+            bool guessed = false;
+
+            while (!guessed)
+            {
+                attempts++;
+                Console.WriteLine($"Попытка No.{attempts}");
+                Console.Write("Введите число от 1 до 1000: ");
+
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Ошибка: введите целое число! Попытка не засчитана.");
+                    attempts--;
+                    continue;
+                }
+
+                if (number < 1 || number > 1000)
+                {
+                    Console.WriteLine("Ошибка: число должно быть от 1 до 1000! Попытка не засчитана.");
+                    attempts--;
+                    continue;
+                }
+                
+
+                if (number == secret) 
+                {
+                    guessed = true;
+                    Console.WriteLine($"\nПоздравляем! Вы угадали число {secret} за {attempts} попыток!");
+                }
+                else if (number < secret)
+                {
+                    Console.WriteLine("Загаданное число БОЛЬШЕ.");
+                }
+                else
+                {
+                    Console.WriteLine("Загаданное число МЕНЬШЕ.");
+                }
+
+            }
+        }
+    }
+}
